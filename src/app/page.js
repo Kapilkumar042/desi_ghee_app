9b05bc2cf97939fab3862bf2456e86f51906cae7
kapilkumar042 <kapilsingh80791@gmail.com>
2024-07-17 21:40:19 +0530

    initilation is complete

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-3xl flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>We are comming soon</h1>
+      
+    </main>
+  );
+}
